b66b11a6a4d7adf70ce0edcd38561b210ffcd20a
FelixKLG <felix@felixklg.dev>
2022-09-28 19:11:58 +0100

    Add data deletion: index.js Clean up index.js and remove some useless code.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,67 @@
- const { PrismaClient } =  require('@prisma/client')
-const { Client, GatewayIntentBits, userMention, User, GuildChannelManager, GuildMessages, MessageContent} = require('discord.js');
-const { token, guildId } = require('../config.json');
+const {PrismaClient} = require('@prisma/client')
+const {
+    Client,
+    GatewayIntentBits,
+    userMention,
+    User,
+    GuildChannelManager,
+    GuildMessages,
+    MessageContent
+} = require('discord.js');
+const {token, guildId} = require('../config.json');
 
 
-
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: ['MESSAGE'] });
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+    partials: ['MESSAGE']
+});
 const prisma = new PrismaClient()
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
- client.on('messageCreate',  async  (message) => {
-    const messageContent = message.content
-     const messagesId = message.id
-        try {const poop = await prisma.messages.create({
-         data: {
-             author: `${message.author.username}` ,
-             messageID: `${messagesId}`,
-             contents: `${messageContent}`
+client.on('messageCreate', async (message) => {
+    await prisma.messages.create({
+        data: {
+            author: message.author.username,
+            messageID: message.id,
+            contents: message.content
+        }
+    }).catch();
+
+    let randomNumber = Math.floor(Math.random() * 21)
+    if (randomNumber === 20) {
+        prisma.messages.delete({
+            where: {
+                messageID: {
+                    not: null
+                }
+            }
+        });
+    }
+});
+
+client.on('messageDelete', async (message) => {
+    const result = await prisma.messages.findFirst({
+        where: {
+            messageID: message.id
+        }
+    })
+    await client.channels.cache
+        .get('1024730889982316554')
+        .send(`a message has just been deleted by: ${result.author} the message contents read: ${result.contents}`);
+})
 
 
-         }
-     })}
-     catch (error){
-        console.log(error)
-     }
-
- });
-
- client.on('messageDelete', async(message) => {
-     const deletedMessageChannel = await client.channels.cache.get('1024730889982316554')
-     const DeltedMessageID = message.id
-     const result =  await prisma.messages.findUnique({
-         where: {
-             messageID: `${DeltedMessageID}`
-         }
-     })
-     await deletedMessageChannel.send(`a message has just been deleted by: ${result.author} the message contents read: ${result.contents}`)
-
-
- })
-
-
-client.on('interactionCreate', async  interaction => {
-    if (interaction.user.id === "500753790388011008") {interaction.reply('fuck off'); return;}
+client.on('interactionCreate', async interaction => {
+    if (interaction.user.id === "500753790388011008") {
+        interaction.reply('fuck off');
+        return;
+    }
     if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction
-
+    const {commandName} = interaction
 
 
     if (commandName === 'ping') {
@@ -57,24 +69,28 @@ client.on('interactionCreate', async  interaction => {
     }
     if (commandName === 'warn') {
         const adminChannel = await client.channels.cache.get('1022839355062104175');
-        const warned_user =  await interaction.options.getUser('target')
+        const warned_user = await interaction.options.getUser('target')
         const reason = await interaction.options.getString('reason')
 
-        await interaction.reply({content:`${warned_user.tag} has been warned by ${interaction.user.tag} for the reason ${reason}`, ephemeral: true})
-        await interaction.followUp({ content:'admins have been notified of your issue', ephemeral: true})
-             .then ( async () => {
-             await client.guilds.cache.get(guildId).channels.create({
-                 name: `${interaction.user.username} issue`,
-                 type: 0
-                 })
-             })
-        try {const poop = await prisma.report.create({
-            data: {
-                userID: `${warned_user.id}`,
-                reason: `${reason}`
-            }
-        })}
-        catch (error) {
+        await interaction.reply({
+            content: `${warned_user.tag} has been warned by ${interaction.user.tag} for the reason ${reason}`,
+            ephemeral: true
+        })
+        await interaction.followUp({content: 'admins have been notified of your issue', ephemeral: true})
+            .then(async () => {
+                await client.guilds.cache.get(guildId).channels.create({
+                    name: `${interaction.user.username} issue`,
+                    type: 0
+                })
+            })
+        try {
+            const poop = await prisma.report.create({
+                data: {
+                    userID: `${warned_user.id}`,
+                    reason: `${reason}`
+                }
+            })
+        } catch (error) {
             console.log(error)
         }
 
@@ -82,16 +98,15 @@ client.on('interactionCreate', async  interaction => {
         await adminChannel.send(`${warned_user.tag} has been warned fat admin bastards attack for ${reason}`)
 
         try {
-            await warned_user.send({content:`You have been warned for ${reason}`, ephemeral: true });
-        }
-        catch (error) {
+            await warned_user.send({content: `You have been warned for ${reason}`, ephemeral: true});
+        } catch (error) {
             console.log(error)
         }
 
 
     }
 
-    if (commandName === 'ban'){
+    if (commandName === 'ban') {
         const user = await interaction.options('user')
         await client.guilds.cache.get(guildId).members.ban(user)
         interaction.reply(`${user.tag} has been banned`)
@@ -101,11 +116,3 @@ client.on('interactionCreate', async  interaction => {
 
 // Login to Discord with your client's token
 client.login(token)
-    .then(async (e) => {
-    await prisma.$disconnect()
-     })
-     .catch(async  (e) => {
-        console.error(e)
-         await prisma.$disconnect()
-       process.exit(1)
-    });
